12825fa5e915cb5eb30406b694e15b64bdc702b4
josh <io@ioio.nu>
2015-04-27 21:24:42 +1000

    move everything into the root directory

--- install/install.sql
+++ install/install.sql
@@ -1,21 +1,21 @@
 -- install.sql
-CREATE TABLE payment (
+CREATE TABLE payments (
   id serial primary key,
   description varchar(256) NOT NULL,
   gateway_id varchar(128),
   amount numeric NOT NULL,
-  user integer NOT NULL
+  userid integer NOT NULL
 );
 
-CREATE TABLE user (
+CREATE TABLE users (
   id serial primary key,
   email varchar(128) NOT NULL UNIQUE,
   name varchar(128) NOT NULL
 );
 
-CREATE TABLE lesson (
-  id serial primary kay,
-  timestamptz NOT NULL,
+CREATE TABLE lessons (
+  id serial primary key,
+  lesson_date timestamptz NOT NULL,
   tutor integer NOT NULL,
   student integer NOT NULL
 );
