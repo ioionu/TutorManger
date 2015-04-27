66ac3e87a6a4474c5e4dc767ca17ff1f789ac6e8
josh <io@ioio.nu>
2015-04-27 21:01:06 +1000

    test db query

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # TutorManger
 CRM for Tutors.
 
+Install
+
 cd tutormanager
 npm install
+
+Run
+
+npm install -g foreman
+nf start
--- tutormanager/routes/index.js
+++ tutormanager/routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var query = require('../db.js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  query('select * from tm', function(err, rows, res) {
+    console.log(rows)
+  });
   res.render('index', { title: 'Express' });
 });
 
