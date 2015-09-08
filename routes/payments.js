44293a46651c47364fde367da25df6d257f67dcd
josh <io@ioio.nu>
2015-09-08 21:05:24 +1000

    server side auth of payment view

--- routes/payments.js
+++ routes/payments.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 /* GET payment index. */
 router.get('/', function(req, res, next) {
-  
+
   if(!req.isAuthenticated()) {
     res.send(401);
   }
@@ -30,27 +30,28 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.post('/', function(req, res, next){
-
-});
-
 /* GET payment instance */
 router.get('/:id', function(req, res, next) {
-  console.log("get payment:", req.params.id);
-  res.query('select * from payments where id = $1::integer;', [req.params.id], function(err, rows, results) {
-    if(err) {
-      console.log("error:", err);
-      res.render('error-db', { message: 'DB Error' });
-    }
-    console.log("row:", rows[0], results);
-    var p = rows[0];
-    if(p.status === 'paid') {
-      p.paid=true;
-    } else {
-      p.paid=false;
-    }
-    res.json(p);
-  });
+  if(!req.isAuthenticated()) {
+    res.send(401);
+  } else {
+
+    console.log("get payment:", req.params.id);
+    res.query('select * from payments where id = $1::integer;', [req.params.id], function(err, rows, results) {
+      if(err) {
+        console.log("error:", err);
+        res.render('error-db', { message: 'DB Error' });
+      }
+      console.log("row:", rows[0], results);
+      var p = rows[0];
+      if(p.status === 'unpaid') {
+        p.unpaid=true;
+      } else {
+        p.unpaid=false;
+      }
+      res.json(p);
+    });
+  }
 });
 
 router.post('/:id', function(req, res, next) {
